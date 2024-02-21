46f1baae5e7a9fae60612cf6af777ddf853d75da
mmuchsin <kangucin21@gmail.com>
2024-02-21 19:14:58 +0700

    feat(components): add Card component with props in App.vue
    Implement the Card component within the App.vue template to display quizzes. The Card component accepts props for quiz name, image, and question count. Also, add a search input to filter quizzes.

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,61 @@
+<script setup>
+import { ref, computed } from 'vue';
+import quizeData from '@/data/quizes.json'
+import Card from './components/Card.vue';
+
+const quizes = ref(quizeData)
+const search = ref('')
+
+const filteredQuizes = computed(() => {
+  if (!search.value) {
+    return quizes.value;
+  }
+  return quizes.value.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()));
+})
+</script>
+
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text" placeholder="Search...">
+      <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div class="card">
-        <img src="https://i.ytimg.com/vi/Kp2bYWRQylk/maxresdefault.jpg" alt="">
-        <div class="card-text">
-          <h2>Math</h2>
-          <p>15 questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in filteredQuizes" :key="quiz.id" :name="quiz.name" :image="quiz.img"
+        :questions="quiz.questions.length" />
     </div>
   </div>
 </template>
 
+
 <style scoped>
-  .container {
-    max-width: 1000px;
-    margin: 0 auto
-  }
+.container {
+  max-width: 1000px;
+  margin: 0 auto
+}
 
-  header {
-    margin-bottom: 10px;
-    margin-top: 30px;
-    display: flex;
-    align-items: center;
-  }
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
 
-  header h1 {
-    font-weight: bold;
-    margin-right: 30px;
-  }
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
 
-  header input {
-    border: none;
-    background-color: rgba(128,128,128,0.1);
-    padding: 10px;
-    border-radius: 5px;
-  }
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
 
-  .options-container {
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 40px;
-  }
-
-  /* CARD */
-
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
 </style>
